763748fc6a98890d3fa2a6125cfb192aa8239bd3
naboatemaa <43883453+naboatemaa@users.noreply.github.com>
2019-05-31 09:34:38 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$(".FLY  ").click(function(){
+ $(".AKUA").toggle();
+});
+
+
+
+//Spell #2
+
+$("    .sleep  ").click(function(){
+ $(".kofi").toggle();
+});
+
+
+
+//Spell #3
+
+$("   .smile   ").click(function(){
+    $(".yaw").toggle();
+ 
+});
+
+
+
